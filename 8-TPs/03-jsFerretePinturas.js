7c0db68b3f7f07435ab0676877d6f886dce336d4
Julián González D'amelio <juliangonzalezdamelio@outlook.com>
2021-02-04 16:39:14 -0300

    Ejercicio 3 del TP (FerretePinturas) resuelto

--- 8-TPs/03-jsFerretePinturas.js
+++ 8-TPs/03-jsFerretePinturas.js
@@ -4,10 +4,33 @@ B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en
 */
 function FahrenheitCentigrados () 
 {
+    let fahrenheit;
+    let centigrados;
+
+    fahrenheit = document.getElementById("txtIdTemperatura").value;
+    fahrenheit = parseFloat(fahrenheit);
+
+    // La fórmula para calcular el paso de °Fahrenheit a °Centígrados es = (°F -32) * 5/9
+
+    centigrados = [(fahrenheit - 32) *5] /9;
+    //centigrados = (fahrenheit - 32) *1.8; No me estaría dando de esta manera
+
+    alert(fahrenheit + " Fahrenheit son " + centigrados + " Centígrados");
 	
 }
 
 function CentigradosFahrenheit () 
 {
+    let centigrados;
+    let fahrenheit;
+
+    centigrados = document.getElementById("txtIdTemperatura").value;
+    centigrados = parseFloat(centigrados);
+
+    // La fórmula para pasar de °C a °F es = (°C * 5/9) + 32. En este caso multipliqué por 1.8, que es lo mismo
+
+    fahrenheit = (centigrados * 1.8) + 32;
+
+    alert(centigrados + " Centígrados son " + fahrenheit + " Fahrenheit");
 	
 }
